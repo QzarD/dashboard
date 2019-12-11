b9560b31ff6145c7dc8286cd9777bd5e2ce9420f
QzarD <a---angel@mail.ru>
2019-12-11 14:30:57 +0700

    Updates

---
+++ precache-manifest.6bda8a71b34c738fe8b77f1a60e30e42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53709652f625f0414fb7348fed731d02",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "88d1c8ee2eec73869a92",
+    "url": "/dashboard/static/css/main.6811674c.chunk.css"
+  },
+  {
+    "revision": "4d553b087e69fad7f2f5",
+    "url": "/dashboard/static/js/2.8bcdbfd4.chunk.js"
+  },
+  {
+    "revision": "88d1c8ee2eec73869a92",
+    "url": "/dashboard/static/js/main.0a81a365.chunk.js"
+  },
+  {
+    "revision": "2995d15d45f754c243c7",
+    "url": "/dashboard/static/js/runtime-main.8bef2b0e.js"
+  }
+]);
